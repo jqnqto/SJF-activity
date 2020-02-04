ac517e2611a8ae5619527d223581631c93b35744
Aj Quinquito <quinquito15@gmail.com>
2020-02-04 16:56:46 +0800

    update table and array

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
-// $('.confirm_appointment').submit(function(e) {
-    
-//     e.preventDefault();
-    
-//     // $(this).serialize(); will be the serialized form
-//     $(this).append($(this).serialize() + '<br />');
-// });
+count = 0;
+    var content = [];
+    function addRow(){
+        count += 1;
+        var job = document.getElementById('job').value;
+        var arrival = document.getElementById('arrival').value;
+        var burst = document.getElementById('burst').value;
+        var table = document.getElementsByTagName('table')[0];
+        var newRow = table.insertRow(1);
 
-// $(document).ready(function(){
-//     $(".add-row").click(function(){
-//         var name = $("#name").val();
-//         var email = $("#email").val();
-//         var markup = "<tr><td><input type='checkbox' name='record'></td><td>" + name + "</td><td>" + email + "</td></tr>";
-//         $("table tbody").append(markup);
-// });
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
 
-function tableUpdate() {
-    if ($("#job").val() != '' &&
-        $("#arrival").val() != null &&
-        $("#burst").val() != null) {
-      // Add product to Table
-      table();
-      // Clear form fields
-      formClear();
-      // Focus to product name field
-      $("#user_form").focus();
+        cell1.innerHTML = job + count;
+        cell2.innerHTML = arrival;
+        cell3.innerHTML = burst;
+
+        content.push({
+            "job": job + count,
+            "arrival": arrival,
+            "burst": burst
+        });
+        var form = document.getElementById("form1").reset();
+        console.log(content);
     }
-  }
-  function formClear() {
-    $("#job").val("");
-    $("#arrival").val("");
-    $("#burst").val("");
-  }
