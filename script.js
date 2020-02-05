03002eda553a5e1563a7aebdb6dd0707b2d99e62
Aj Quinquito <quinquito15@gmail.com>
2020-02-05 17:07:25 +0800

    update textfield

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 count = 0;
-    var content = [];
-    function addRow(){
-        count += 1;
+var content = [];
+
+function validateForm() {
+    var isEmpty = false;
+    var arrival = document.getElementById('arrival').value;
+    var burst = document.getElementById('burst').value;
+
+    if(arrival===""){
+        alert("Please enter arrival.");
+        isEmpty = true;
+    } else if (burst===""){
+        alert("Please enter burst.");
+        isEmpty = true;
+    }
+    return isEmpty;
+    
+}
+
+function addRow(){
+    if(!validateForm()){
+        count += 1; 
         var job = document.getElementById('job').value;
         var arrival = document.getElementById('arrival').value;
         var burst = document.getElementById('burst').value;
         var table = document.getElementsByTagName('table')[0];
         var newRow = table.insertRow(1);
-
+        
         var cell1 = newRow.insertCell(0);
         var cell2 = newRow.insertCell(1);
         var cell3 = newRow.insertCell(2);
@@ -21,6 +39,9 @@ count = 0;
             "arrival": arrival,
             "burst": burst
         });
-        var form = document.getElementById("form1").reset();
+        var form = document.getElementById("form1").reset(); 
         console.log(content);
     }
+        
+}
+
